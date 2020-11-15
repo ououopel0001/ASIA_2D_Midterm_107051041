87e348f9e2144fbe41bc39d3163716960d1c2df5
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2020-11-15 15:42:48 +0800

    加入程式

---
+++ 2D_Midterm_107051041/Assets/scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject final;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("踩到傳送門囉!");
+    }
+}
